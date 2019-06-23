f5e0f9051b828c66c95172bc02625c36e7029263
Tom Still <tom.still@live.co.uk>
2019-06-23 16:44:14 +0100

    Create numeric_overflow_error_fix.sql

---
+++ numeric_overflow_error_fix.sql
@@ -0,0 +1,3 @@
+/* When you recieve the "numeric overflow error", it means you are trying to jam in a very large number into a small space */
+/* To fix this; set the column datatype to numeric(5,2); instead of numeric(4,2); */
+/* The data will remain correct with this change */
